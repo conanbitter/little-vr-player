322a49589fa6aecb7f4e8a3bb55ef20f5dd03dd6
Conan Bitter <conan.bitter@gmail.com>
2020-08-07 22:32:06 +0300

    Minimal SDL+OpenGL app code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,53 @@
+#define SDL_MAIN_HANDLED
+
+#include <SDL.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+SDL_Window *window;
+SDL_GLContext context;
+
 int main(int argc, char **argv) {
+    cout << "Init..." << endl;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    window = SDL_CreateWindow("Little VR player", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+    context = SDL_GL_CreateContext(window);
+    if (context == NULL) {
+        cout << "Error creating context" << endl;
+    }
+
+    SDL_Event event;
+    bool working = true;
+    cout << "Run..." << endl;
+    while (working) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    working = false;
+                    break;
+            }
+        }
+
+        SDL_GL_SwapWindow(window);
+        SDL_Delay(5);
+    }
+
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
